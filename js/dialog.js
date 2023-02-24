4893d3463ff7d267297369bf9a0524576b3b707a
L_Emanuel <43679743+Luis-Emanuel@users.noreply.github.com>
2023-02-24 11:37:43 -0300

    Carrossel finalizado

--- js/dialog.js
+++ js/dialog.js
@@ -1,19 +1,34 @@
 // Variáveis
+var conteudoForaDialog = document.querySelector('#conteudoForaDialog');
 var btnAbreDialog = document.querySelector('#abreDialog');
 var dialog = document.querySelector('.dialogNewsletter');
 var dialogBody = document.querySelector('.dialogNewsletter-body');
 var dialogOverlay = document.querySelector('.dialogNewsletter-overlay');
 
+btnAbreDialog.style.display = 'block';
 
 // Quando abrir a dialog...
 btnAbreDialog.addEventListener('click', function() {
   dialog.classList.add('dialogNewsletter--aberto');
+  document.querySelector('.dialogNewsletter-campo').focus();
+  conteudoForaDialog.inert = true;
 });
 
 function fechandoDialog() {
   document.activeElement.blur();
-  dialog.classList.remove('dialogNewsletter--aberto');     
+  dialog.classList.remove('dialogNewsletter--aberto');   
+  conteudoForaDialog.inert = false;
+  btnAbreDialog.focus();
+
 }
 
 // Listeners
 document.querySelector('.dialogNewsletter-fechar').addEventListener('click', fechandoDialog);
+
+dialogOverlay.addEventListener('click', fechandoDialog)
+
+document.addEventListener('keyup', function(evento){
+  if(evento.keyCode == 27){
+    fechandoDialog()
+  }
+})
